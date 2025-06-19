1d69251c249dfe10c1e3bc8282b76961b067e305
Raghav Agarwal <raghx@Raghavs-MacBook-M2-Air.local>
2025-06-19 07:45:32 +0530

    Updated contact page, navbar href, and HTML styling

--- app.py
+++ app.py
@@ -2,14 +2,18 @@ from flask import Flask,render_template,request
 import pickle
 import pandas as pd
 import numpy as np
+import random
 
 
 app = Flask(__name__)
 
-popular_books = pickle.load(open('popular_books_final_with_info.pkl','rb'))
+popular_books = pickle.load(open('Popular_books_final_with_info.pkl','rb'))
 similarity_scores = pickle.load(open('Similarity_score.pkl','rb'))
 books = pickle.load(open('Books.pkl','rb'))
 table = pickle.load(open('table.pkl','rb'))
+
+
+
 
 @app.route('/')
 def index():
@@ -21,17 +25,24 @@ def index():
                            rating = list(popular_books['AverageRating'].values)
                           
                            )
-                           
+
+
+
+
 @app.route('/recommend')
 def recommend_ui():
-    return render_template('recommend.html')
+    sample_books = random.sample(list(table.index), 10)  
+    return render_template('recommend.html', suggestions=sample_books)
+
+
+
 
 @app.route('/recommend_books', methods=['POST'])
 def recommend():
     book_name = request.form.get('user_input')  
 
     if book_name not in table.index:
-        return render_template('recommend.html', data=[], error=f"'{book_name}' not found. Please try another book.")
+        return render_template('recommend.html', not_found=True)
 
     index = np.where(table.index == book_name)[0][0]
     similar_books = sorted(
@@ -55,6 +66,10 @@ def recommend():
     return render_template('recommend.html', data=data)
 
 
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 
 if __name__ == '__main__':
